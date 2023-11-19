c791d0da2a5652609ad3a60b786ba7ab63952d97
Saúl Camacho <electrohead1200@gmail.com>
2023-11-19 13:42:54 -0600

    Componente para agregar Categorías y mostrarlas en una lista

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+import { AddCategory } from './components/AddCategory';
+
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch', 'DragonBall'])
+
+    const onAddCategory = (newCategory) => {
+        if (categories.includes(newCategory)) return;
+        setCategories([...categories, newCategory]);
+    }
     return (
-      <>
-        <h1>GifExpertApp</h1>
-      </>      
+        <>
+            {/* Titulo */}
+            <h1>GifExpertApp</h1>
+            
+            {/* Input */}
+            <AddCategory
+            // setCategories={setCategories} 
+                onNewCategory = {(value) => onAddCategory(value)}
+            />
+
+            {/* Listado de Gif */}
+
+            <ol>
+                {
+                    categories.map(category => {
+                        return <li key={category}>{ category }</li>
+                    })
+                }
+            </ol>
+
+        </>      
   )
 }
