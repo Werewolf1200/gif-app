4dbe2b3dfd70c180e8ff09207dc497617a6bb476
Saúl Camacho <electrohead1200@gmail.com>
2023-11-22 13:27:14 -0600

    Componente para mostrar los Gifs en Card y un Grid, con el titulo

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,16 +1,35 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { GifItem } from './GifItem';
 import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
 
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  }
+
   // Disparar efectos secuandarios: un proceso que se quiere dispara cuando algo suceda
   useEffect(() => {
-    getGifs(category);
+    getImages(category);
   }, [])
   
     return (
       <>
-            <h3>{category}</h3>
+        <h3>{category}</h3>
+
+        <div className='card-grid'>
+          {
+            images.map((image) => (
+              <GifItem 
+                key={image.id}
+                {...image}// Expandir props
+              />
+                ))
+          }
+        </div>
       </>
   )
 }
